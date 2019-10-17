e09c94074728959a2420285b783f9fa159655d88
Khaled Mohamed <kmohamed@g2o.com>
2019-10-17 10:23:06 -0400

    add login module

--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vuelidate from 'vuelidate';
+//import { router } from '@/_helpers';
+
+Vue.use(Vuelidate);
 Vue.config.productionTip = false;
 
+// setup fake backend
+//import { configureFakeBackend } from './_helpers';
+//configureFakeBackend();
+
 new Vue({
+    el: '#app',
     router,
     store,
     render: h => h(App)
-}).$mount('#app');
+});
--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Home from './views/Home.vue'
-// import AccountsList from './components/accounts/list'
+
+import { authenticationService } from '@/_services';
+import { Role } from '@/_helpers';
+
 import BalanceSheet from './components/balance-sheet/balance-sheet'
 import TrialBalance from './components/trial-balance/trial-balance'
 import journalsList from './components/journals/journals-list'
@@ -9,48 +11,60 @@ import addJournal from './components/journals/add-journal'
 import generalLedger from './components/general-ledger/general-ledger'
 import AccountsList from './components/accounts/list';
 import addAccount from './components/accounts/add-account';
-import Login from './components/login'
+import LoginPage from '@/components/security/LoginPage';
+
 
 Vue.use(Router);
 
-export default new Router({
+export const router = new Router({
+    mode: 'history',
     routes: [
-        { path: '/login', component: Login },
+        { 
+            path: '/login', 
+            component: LoginPage 
+        },
         {
             path: '/',
             name: 'home',
-            component: BalanceSheet
+            component: BalanceSheet,
+            meta: { authorize: [] } 
         },
         {
             path: '/accounts/add',
             name: 'add-accounts',
-            component: addAccount
+            component: addAccount,
+            meta: { authorize: [Role.Admin] } 
         },
         {
             path: '/accounts',
             name: 'accounts',
-            component: AccountsList
+            component: AccountsList,
+            meta: { authorize: [] } 
         },
         {
             path: '/balance-sheet',
             name: '/balance-sheet',
-            component: BalanceSheet
+            component: BalanceSheet,
+            meta: { authorize: [] } 
         },
         {
             path: '/trial-balance',
             name: '/trial-balance',
-            component: TrialBalance
+            component: TrialBalance,
+            meta: { authorize: [] } 
         },
 
         {
             path: '/journals',
             name: '/journals',
-            component: journalsList
+            component: journalsList,
+            meta: { authorize: [] } 
         },
         {
             path: '/journals/add',
             name: '/addJournal',
-            component: addJournal
+            component: addJournal,
+            meta: { authorize: [] } 
         },
         {
             path: '/general-ledger',
@@ -67,15 +81,27 @@ export default new Router({
         }
     ]
 });
-//     .beforeEach((to, from, next) => {
-//     // redirect to login page if not logged in and trying to access a restricted page
-//     const publicPages = ['/login'];
-//     const authRequired = !publicPages.includes(to.path);
-//     const loggedIn = localStorage.getItem('user');
-//
-//     if (authRequired && !loggedIn) {
-//         return next('/login');
-//     }
-//
-//     next();
-// });
+
+router.beforeEach((to, from, next) => {
+    // redirect to login page if not logged in and trying to access a restricted page
+    const { authorize } = to.meta;
+    const currentUser = authenticationService.currentUserValue;
+
+    if (authorize) {
+        if (!currentUser) {
+            // not logged in so redirect to login page with the return url
+            return next({ path: '/login', query: { returnUrl: to.path } });
+        }
+
+        // check if route is restricted by role
+        if (authorize.length && !authorize.includes(currentUser.role)) {
+            // role not authorised so redirect to home page
+            return next({ path: '/' });
+        }
+    }
+
+    next();
+});
+
+export default router;
+
